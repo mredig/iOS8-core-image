24ea8feb39bb6e2b6ddca22affede3f9530b739d
Michael Redig <mredig@gmail.com>
2019-09-30 11:56:29 -0500

    designed ui and connected to code

---
+++ iOS8 Photo Filter/iOS8 Photo Filter/ImageFilterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ImageFilterViewController.swift
+//  iOS8 Photo Filter
+//
+//  Created by Michael Redig on 9/30/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+import UIKit
+
+class ImageFilterViewController: UIViewController {
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var brightnessSlider: UISlider!
+	@IBOutlet var contrastSlider: UISlider!
+	@IBOutlet var saturationSlider: UISlider!
+
+
+	@IBAction func brightnessChanged(_ sender: UISlider) {
+	}
+
+	@IBAction func contrastChanged(_ sender: UISlider) {
+	}
+
+	@IBAction func saturationChanged(_ sender: UISlider) {
+	}
+
+	@IBAction func choosePhotoPressed(_ sender: UIBarButtonItem) {
+	}
+
+	@IBAction func savePhotoPressed(_ sender: UIButton) {
+	}
+
+}
+
